f862bb61576a7bb5fb9976ca34dbf1e6629648b6
hp-liao <jiaguo.liao@hp.com>
2015-04-06 09:38:36 +0800

    rm intense files

--- emagazine/client/default/lua/dataOp.lua
+++
@@ -1,8 +0,0 @@
-
-local apiHelper = require "apiHelper"
-
-module( ..., package.seeall ) 
-
-function EBookGet( MinCreateTime ,MinShareInfoCreateTime, whichBook, DeviceType )
-	return apiHelper.EBookGet(MinCreateTime,MinShareInfoCreateTime,whichBook, DeviceType)
-end
